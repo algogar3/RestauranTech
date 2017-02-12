4a75c02e796744adcc76252f657e72131559339e
javiersanz <javi95sanz@gmail.com>
2017-02-12 11:30:44 +0100

    Panel logo ajustado. Logo modificado.

--- src/vistas/PanelLogo.java
+++ src/vistas/PanelLogo.java
@@ -1,22 +1,55 @@
 package vistas;
 
 import java.awt.Dimension;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Scrollable;
 
-public class PanelLogo extends JPanel {
+public class PanelLogo extends JPanel implements Scrollable{
 	
 	// Constructor
 	public PanelLogo(){
 		iniciarLogo();
-		this.setVisible(true);
+		setPreferredSize(new Dimension(590,590));
+		setVisible(true);
 	}
 	
 	public void iniciarLogo(){
 		JLabel labelLogo = new JLabel(new ImageIcon("img/logo.jpg"));
 		add(labelLogo);
+	}
+
+	@Override
+	public Dimension getPreferredScrollableViewportSize() {
+		// TODO Auto-generated method stub
+		return getPreferredSize();
+	}
+
+	@Override
+	public int getScrollableBlockIncrement(Rectangle arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportHeight() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportWidth() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int getScrollableUnitIncrement(Rectangle arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
